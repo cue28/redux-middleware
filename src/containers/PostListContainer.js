7a6051a2be096dbdf8cc98dd77c685471f5e4538
Siyoon Jeon <jeoncue@gmail.com>
2021-11-10 15:38:03 +0900

    processing reloading

--- src/containers/PostListContainer.js
+++ src/containers/PostListContainer.js
@@ -8,13 +8,21 @@ function PostListContainer() {
   const dispatch = useDispatch();
 
   // 컴포넌트 마운트 후 포스트 목록 요청
+  // 뒤로 가기 시에도 로딩중이 보여 사용자에게 좋지 못한 경험을 보여주게 된다.
   useEffect(() => {
     dispatch(getPosts());
   }, [dispatch]);
 
-  if (loading) return <div>로딩중...</div>;
+  //포스트 목록이 이미 있는데 재로딩하는 이슈 수정 첫번째 방법
+  // useEffect(() => {
+  //   if (data) return;
+  //   dispatch(getPosts());
+  // }, [data, dispatch]);
+
+  if (loading && !data) return <div>로딩중...</div>; // 로딩중이면서, 데이터가 없을 때에만 로딩중... 표시
   if (error) return <div>에러 발생!</div>;
   if (!data) return null;
+  
   return <PostList posts={data} />;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,28 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './modules';
-// import myLogger from './middlewares/myLogger';
-import logger from 'redux-logger'; //리덕스 관련 정보들 콘솔에 출력
+import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
+import { BrowserRouter } from 'react-router-dom';
 
 const store = createStore(
   rootReducer,
+  // logger 를 사용하는 경우, logger가 가장 마지막에 와야합니다.
   composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 ); // 여러개의 미들웨어를 적용 할 수 있습니다.
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
